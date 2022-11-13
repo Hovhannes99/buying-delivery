195b7a9f1a39a641d8861ebf182ab820d75c0b91
Hovhannes99 <hovoohanjanyan9@gmail.com>
2022-11-14 00:37:42 +0400

    feat:menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Header from "./pages/Header";
+import Header from "./pages/header";
+import MenuBar from "./components/molecules/menuBar";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <MenuBar/>
     </div>
   );
 }
---
+++ src/components/molecules/menuBar.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+type Anchor = 'menu'
+
+
+const MenuBar =() => {
+    const [state, setState] = React.useState({
+        menu: false,
+    });
+
+    const toggleDrawer = (anchor: Anchor, open: boolean) =>
+            (event: React.KeyboardEvent | React.MouseEvent) => {
+                if (
+                    event.type === 'keydown' &&
+                    ((event as React.KeyboardEvent).key === 'Tab' ||
+                        (event as React.KeyboardEvent).key === 'Shift')
+                ) {
+                    return;
+                }
+
+                setState({ ...state, [anchor]: open });
+            };
+
+    const list = (anchor: Anchor) => (
+        <Box
+            sx={{ width: anchor === 'menu' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+    return (
+        <div>
+                    <Button onClick={toggleDrawer("menu", true)}>Menu</Button>
+                    <Drawer
+                        anchor={"left"}
+                        open={state["menu"]}
+                        onClose={toggleDrawer("menu", false)}
+                    >
+                        {list("menu")}
+                    </Drawer>
+        </div>
+    );
+}
+
+export default MenuBar
